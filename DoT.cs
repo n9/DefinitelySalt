b7a80b28a6b1692470db0ba7dd4c02830489123c
n9 <n9>
2014-10-06 10:01:55 +0200

    wip

---
+++ DoT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace DefinitelySalt
+{
+    [Imported]
+    [IgnoreNamespace]
+    [ScriptName("doT")]
+    public static class DoT
+    {
+        public static DotTemplateSettings TemplateSettings;
+
+
+    }
+
+    [Imported]
+    [Serializable]
+    public class DotTemplateSettings
+    {
+
+    }
+}
--- Handlebars.cs
+++ Handlebars.cs
@@ -14,12 +14,21 @@ namespace DefinitelySalt
         public JsDictionary<string, Delegate> Partials;
     }
 
+    [Imported]
+    [Serializable]
+    public class HandlebarsPrecompiledTemplate
+    {
+
+    }
+
     public delegate string HandlerbarsTemplate(JsDictionary<string, object> context, HandlebarsOptions options = null);
 
     [Imported]
     [IgnoreNamespace]
     public static class Handlebars
     {
-        public static HandlerbarsTemplate Compile(string source) { return null; }
+        public static HandlerbarsTemplate Compile(string source, HandlebarsOptions options = null) { return null; }
+        public static string Precompile(string source, HandlebarsOptions options = null) { return null; }
+        public static HandlerbarsTemplate Template(HandlebarsPrecompiledTemplate precompiled) { return null; }
     }
 }
