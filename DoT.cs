7966f3ad56cf613007ca175d87ff7868e8af4c3f
n9 <n9>
2015-07-03 18:56:05 +0200

    wip doT

--- DoT.cs
+++ DoT.cs
@@ -6,32 +6,43 @@ using System.Runtime.CompilerServices;
 namespace DefinitelySalt
 {
     [Imported]
-    public interface IDoTOp
+    [ScriptNamespace("doT")]
+    [ScriptName("Context")]
+    public class DoTContext
     {
         [ScriptName("i")]
-        string Interpolate(object value, string param);
+        public extern virtual string Interpolate(object value, string param);
         
         [ScriptName("c")]
-        bool Condition(object value);
+        public extern virtual bool Condition(object value);
         
         [ScriptName("l")]
-        string Loop(object value, DoTItemWriter itemWriter, DoTSeparatorWriter separatorWriter);
+        public extern virtual string Loop(object value, DoTItemWriter itemWriter, DoTSeparatorWriter separatorWriter);
 
         [ScriptName("b")]
-        string Block(string name, object jsArguments, JsDictionary<string, DoTTemplate> templateArguments);
+        public extern virtual string Block(string name, object jsArguments, JsDictionary<string, DoTTemplate> templateArguments);
+
+        [ScriptName("ib")]
+        public extern virtual Func<object, Func<DoTContext, JsDictionary<string, DoTTemplate>>> InlineBlock(JsDictionary<string, DoTTemplate> defaultTemplateArguments);
 
         [ScriptName("bm")]
-        object BlockMeta(string name, object jsArguments);
+        public extern virtual object BlockMeta(string name, object jsArguments);
+
+        public extern virtual string UnknownBlock(string name, object jsArguments, JsDictionary<string, DoTTemplate> templateArguments);
+        public extern virtual object UnknownBlockMeta(string name, object jsArguments);
+        public extern virtual DoTContext MayDerive(JsDictionary<string, DoTTemplate> blocks);
+
+        public extern virtual DoTContext FullDerive();
     }
 
     [BindThisToFirstParameter]
-    public delegate string DoTBlockFunc(IDoTOp op, string name, object jsArguments, JsDictionary<string, DoTTemplate> templateArguments);
+    public delegate string DoTBlockFunc(DoTContext c, string name, object jsArguments, JsDictionary<string, DoTTemplate> templateArguments);
 
-    public delegate string DoTTemplate(IDoTOp op, object data);
+    public delegate string DoTTemplate(DoTContext c, object data);
 
-    public delegate string DoTItemWriter(IDoTOp op, object item, int index, object[] collection);
+    public delegate string DoTItemWriter(DoTContext c, object item, int index, object[] collection);
 
-    public delegate string DoTSeparatorWriter(IDoTOp op);
+    public delegate string DoTSeparatorWriter(DoTContext c);
 
     [Imported]
     [IgnoreNamespace]
@@ -44,14 +55,16 @@ namespace DefinitelySalt
         public static extern string Template(string source, DotTemplateSettings settings = null);
 
         public static extern string EncodeHTML(object value);
+        public static extern Function ConstructorFromInstance(object instance);
 
         [InlineCode("{$DefinitelySalt.DoT}.loop.call({op}, {value}, {itemWriter}, {separatorWriter})")]
-        public static extern string Loop(IDoTOp op, object value, DoTItemWriter itemWriter, DoTSeparatorWriter separatorWriter);
-        
+        public static extern string Loop(DoTContext op, object value, DoTItemWriter itemWriter, DoTSeparatorWriter separatorWriter);
+
         [InlineCode("{$DefinitelySalt.DoT}.blockSplatter.call({op}, {name}, {jsArguments}, {templateArguments}, {blockFunction})")]
-        public static extern string BlockSplatter(IDoTOp op, string name, object jsArguments,
+        public static extern string BlockSplatter(DoTContext op, string name, object jsArguments,
             JsDictionary<string, DoTTemplate> templateArguments,
             DoTBlockFunc blockFunction);
+
     }
 
     [Imported]
