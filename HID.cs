0dcce309ab91402c2f31ef4701fa65eef9f2754b
n9 <n9>
2016-12-18 18:19:28 +0100

    wip node-hid

---
+++ HID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Framework.Core;
+using System.Runtime.CompilerServices;
+using System.Collections.TypedArrays;
+using NodeJS.EventsModule;
+
+namespace DefinitelySalt
+{
+    [Imported]
+    [IgnoreNamespace]
+    [ScriptName("HID")]
+    [ModuleName("node-hid")]
+    public class Hid : EventEmitter
+    {
+        public extern Hid(string path);
+        public extern Hid(int vendorId, int productId);
+        public extern void Close();
+    }
+}
