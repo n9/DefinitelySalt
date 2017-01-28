13fe9198f2a0b4e09aa1cb40cc752bbb62c2bc90
n9 <n9>
2017-01-28 18:09:01 +0100

    wip PsTree

--- HID.cs
+++ HID.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Framework.Core;
 using System.Runtime.CompilerServices;
 using System.Collections.TypedArrays;
 using NodeJS.EventsModule;
---
+++ PsTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DefinitelySalt
+{
+    [IgnoreNamespace]
+    [Imported]
+    public static class PsTree
+    {
+        [InlineCode("require('ps-tree')({pid}, {action})")]
+        public static extern void For(int pid, Action<Error, List<PsTreeProcessInfo>> action);
+    }
+
+    [Imported]
+    [Serializable]
+    public class PsTreeProcessInfo
+    {
+        [ScriptName("PID")]
+        public string Pid;
+
+        // ...
+    }
+}
